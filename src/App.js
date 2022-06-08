177612f7781d673716b881709c336f9523dbfb6b
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-06-09 02:38:46 +0400

    add player 1 and 2 cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 
 function App() {  
   const[bool, setBool] = useState(true)
-  
   const [matrix,setMatrix] = useState([
     [0,0,0,0,0,0],
     [0,0,0,0,0,0],
@@ -11,6 +10,9 @@ function App() {
     [0,0,0,0,0,0],
     [0,0,0,0,0,0]
   ])
+  const[pl1Card, setPl1Card] = useState(matrix.length * matrix[0].length/2)
+  const[pl2Card, setPl2Card] = useState(matrix.length * matrix[0].length/2)
+  
     
   const handleClick =(id)=>{
     for(let i = 4; i>= 0;i--){
@@ -19,6 +21,9 @@ function App() {
         newMatrix[i][id] = bool ? 1 : 2   
         setMatrix([...newMatrix])
         setBool(!bool)
+
+        matrix[i][id]===1 ? setPl1Card(item=>item-1) : setPl2Card(item=>item-1)
+
         break
       }
     }
@@ -30,18 +35,36 @@ function App() {
 
   return (
     <div className="App">
-      <div>   
+      <div className='buttonsWrapper'>   
         {bottons}
       </div>
-      {
-        matrix?.map((row,idxRow)=>   
-            <div key={idxRow}>
-              {row?.map((call,idxCall)=>
-                <div className={`default_${call}`} key={idxCall}></div>              
-              )}
-            </div>
-          )
-      }
+      <div className='contentWrapper'>
+        <div className='player1'>
+          { 
+            [...Array(pl1Card)].map((item,id) => 
+              <span key={id}></span> 
+            )  
+          }
+        </div>
+        <div className='mainBord'>
+          {
+            matrix?.map((row,idxRow)=>   
+                <div key={idxRow}>
+                  {row?.map((call,idxCall)=>
+                    <div className={`default_${call}`} key={idxCall}></div>              
+                  )}
+                </div>
+              )
+          }
+        </div>
+        <div className='player2'>
+          { 
+            [...Array(pl2Card)].map((item,id) => 
+              <span key={id}></span> 
+            )  
+          }
+        </div>
+      </div>
     </div>
   );
 }
