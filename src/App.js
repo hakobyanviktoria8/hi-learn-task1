6ea51f57c0dca678897f286e29d917edb37d5cb5
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-06-08 11:42:47 +0400

    test curent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+const[bool, setBool] = useState(true)
+
+const [matrix,setMatrix] = useState([
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0]
+])
+
+const handleClick =(id)=>{
+  for(let i = 4; i>= 0;i--){
+    if(matrix[i][id] === 0){
+      let newMatrix = matrix;
+      newMatrix[i][id] = bool ? 1 : 2   
+      setMatrix([...newMatrix])
+      setBool(!bool)
+      break
+    }
+  }
+}
+console.log("matrix", matrix)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>            
+        <button onClick={e=>handleClick(0)}>+</button>       
+        <button onClick={e=>handleClick(1)}>+</button>       
+        <button onClick={e=>handleClick(2)}>+</button>       
+        <button onClick={e=>handleClick(3)}>+</button>       
+        <button onClick={e=>handleClick(4)}>+</button>       
+      </div>
+      {
+        matrix?.map((row,idxRow)=>   
+            <div key={idxRow}>
+              {row?.map((call,idxCall)=>
+                <div className={`default_${call}`} key={idxCall}></div>              
+              )}
+            </div>
+          )
+      }
     </div>
   );
 }
